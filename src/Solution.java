2265f3d38cd862ae430f2630f96af64aee4de6d6
Raving-hash <Raving-hash@foxmail.com>
2021-12-27 10:48:26 +0800

    快速幂法，每次便利都是二分法的运用

--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n == 0) {
+        if (n == 0) {
             return 1;
-        }else if (x == 0){
-            return 0;
-        }else if (n == 1){
-            return x;
-        }else if(n == -1){
-            return 1/x;
-        }else if(x == 1){
-            return 1;
-        }else if(x == -1){
-            if((n & 1) == 0){
-                return 1;
-            }else{
-                return -1;
-            }
-        }else if (n == -2147483648){
+        } else if (x == 0) {
             return 0;
         }
-        double res = x;
-        if (n < 0){
-             res = compute(x,-n);
-             res = 1 / res;
-        }else{
-             res = compute(x,n);
+        double res = 1.0;
+        long m = n;
+
+        if (m < 0) {
+            m = -m;
+            x = 1 / x;
         }
-        return res;
-    }
-
-    public double compute(double x, int n){
-        int m = n;
-        double res = x;
-        if((n & 1) == 0){
-            while (m > (n/2 + 1)){
+        while (m > 0) {
+            if ((m & 1) == 1) {
                 res *= x;
-                m--;
-                if(res >= 10000){
-                    return 0;
-                }
             }
-            res *= res;
-
-        }else{
-            while (m > (n/2 + 2)  ){
-                res *= x;
-                if(res >= 10000){
-                    return 0;
-                }
-                m--;
-            }
-            res = res * res * x;
-
+            x *= x;
+            m = m >> 1;
         }
         return res;
     }
